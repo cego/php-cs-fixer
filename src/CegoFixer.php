af2bad17e4245796a2edba9288088e5c6d5e2cd6
Niki Ewald Zakariassen <niza@cego.dk>
2021-09-09 15:36:09 +0200

    Added option for custom fixers

--- src/CegoFixer.php
+++ src/CegoFixer.php
@@ -2,8 +2,10 @@
 
 namespace Cego;
 
+use Traversable;
 use PhpCsFixer\Finder;
 use PhpCsFixer\Config;
+use PhpCsFixer\Fixer\FixerInterface;
 
 class CegoFixer
 {
@@ -13,16 +15,18 @@ class CegoFixer
      * @param Finder $finder
      * @param array $customRules
      * @param false $isRiskyAllowed
+     * @param FixerInterface[]|iterable|Traversable $customFixers
      *
      * @return Config
      */
-    public static function applyRules(Finder $finder, array $customRules = [], bool $isRiskyAllowed = false): Config
+    public static function applyRules(Finder $finder, array $customRules = [], bool $isRiskyAllowed = false, iterable $customFixers = []): Config
     {
         return (new Config())
             ->setFinder($finder)
             ->setRiskyAllowed($isRiskyAllowed)
             ->setHideProgress(false)
             ->setUsingCache(true)
+            ->registerCustomFixers($customFixers)
             ->setRules(array_merge(CegoRuleSet::get(), $customRules));
     }
 }
