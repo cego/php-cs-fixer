119a12b4b2a806d0c08fede837041b65d99a6ba2
Mads Jon Nielsen <madsjon@gmail.com>
2024-02-27 11:01:17 +0100

    Revert "Added rule for alignment of multiline parameters"

--- src/CegoFixer.php
+++ src/CegoFixer.php
@@ -5,7 +5,6 @@ namespace Cego;
 use Traversable;
 use PhpCsFixer\Finder;
 use PhpCsFixer\Config;
-use ErickSkrauch\PhpCsFixer\Fixers;
 use PhpCsFixer\Fixer\FixerInterface;
 
 class CegoFixer
@@ -28,7 +27,6 @@ class CegoFixer
             ->setHideProgress(false)
             ->setUsingCache(true)
             ->registerCustomFixers($customFixers)
-            ->registerCustomFixers(new Fixers())
             ->setRules(array_merge(CegoRuleSet::get(), $customRules));
     }
 }
